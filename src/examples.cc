14b99abe51e88a1c350f49c8e95aa9935dafabbe
DavisVaughan <davis@rstudio.com>
2020-10-12 21:58:19 -0400

    Add basic example

--- src/examples.cc
+++ src/examples.cc
@@ -1,5 +1,16 @@
 #include "examples.h"
+#include <cctz/civil_time.h>
 
 SEXP cctz_test() {
-  return R_NilValue;
+  cctz::civil_day d(2016, 2, 1);
+
+  SEXP out = PROTECT(Rf_allocVector(INTSXP, 3));
+  int* p_out = INTEGER(out);
+
+  p_out[0] = d.year();
+  p_out[1] = d.month();
+  p_out[2] = d.day();
+
+  UNPROTECT(1);
+  return out;
 }
